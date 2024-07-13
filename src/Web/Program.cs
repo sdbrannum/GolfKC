d6e64bff8c1cd5b8b4de17746ff787ba7b961269
Steven Brannum <sdbrannum@gmail.com>
2024-07-12 23:49:28 -0500

    docs: cleanup http file and client readme

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -85,8 +85,3 @@ app.MapGet("{source}/tee-times/{courseId}", [ResponseCache(Duration = 60)] async
 
 app.MapRazorPages();
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
-}
