130381b47eb48c6be447481ea7a6d7ad66a6177d
sdbrannum <sdbrannum@gmail.com>
2026-04-26 19:01:08 -0500

    cleanup

--- src/Web/Client/src/App.vue
+++ src/Web/Client/src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, watch, onMounted } from "vue";
+import { ref, watch } from "vue";
 import { getCourses } from "./api";
 import CourseCard from "./components/CourseCard.vue";
 import RadioButtons from "./components/RadioButtons.vue";
