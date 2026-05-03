93cd3ec51c0f7c6e68a58d0ebe8e389105b8b096
sdbrannum <sdbrannum@gmail.com>
2026-05-03 13:12:58 -0500

    fix: golfnow requires searchtype as string now

--- src/Web/Integrations/Dtos/GolfNowTeeTimesRequest.cs
+++ src/Web/Integrations/Dtos/GolfNowTeeTimesRequest.cs
@@ -2,12 +2,12 @@ namespace Web.Integrations.Dtos;
 
 public class GolfNowTeeTimesRequest
 {
-    public uint Radius { get; init; } = 25;
-    public double Latitude { get; init; }
-    public double Longitude { get; set; }
+    public uint Radius { get; } = 30;
+    public double Latitude { get; } = 39.01470395078284;
+    public double Longitude { get; } = -94.69324882561136;
     public int PageSize { get; } = 30;
     public int PageNumber { get; } = 0;
-    public int SearchType { get; } = 1;
+    public string SearchType { get; } = "Facility";
     public string SortBy { get; } = "Date";
     public int SortDirection { get; } = 0;
     public string Date { get; set; } = default!;
